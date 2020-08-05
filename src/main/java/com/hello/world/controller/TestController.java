6f762213f52441cd6e40c0548abd124bc308cf12
h4ppyy <93immm@naver.com>
2020-08-05 16:18:06 +0900

    model test

--- src/main/java/com/hello/world/controller/TestController.java
+++ src/main/java/com/hello/world/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.hello.world.controller;
 
+import javax.validation.Valid;
+
+import com.hello.world.model.TestModel;
 import com.hello.world.service.TestService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +21,7 @@ public class TestController {
 
   @ResponseBody
   @RequestMapping(value="/hello", method=RequestMethod.GET)
-  public String test( Model model ) {
+  public String test( Model model, TestModel testModel ) {
     return testService.test();
   }
 }
---
+++ src/main/java/com/hello/world/model/TestModel.java
@@ -0,0 +1,14 @@
+package com.hello.world.model;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+public class TestModel {
+
+  private String sample1;
+
+  private String sample2;
+
+}
